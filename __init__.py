7b6033ef7826658014734784ec4bdbc63bce7558
kiwi <cj6fu6jo25@gmail.com>
2024-09-01 16:22:27 -0700

    20240901 deployment

--- __init__.py
+++ __init__.py
@@ -41,3 +41,7 @@ def project(slug):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
